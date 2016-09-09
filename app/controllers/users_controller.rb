dbd4df4594ee0f25c1c107b37cdaeda04999bca3
matsuda tomoki <t.m.0011223344@gmail.com>
2016-09-09 17:28:20 +0900

    Gather_into_each_user(@user=User.find(params[:id]))

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,15 +2,15 @@ class UsersController < ApplicationController
 
 
   def show
-    @user = User.find(params[:id])
+    each_user
   end
 
   def edit
-    @user = User.find(params[:id])
+    each_user
   end
 
   def update
-    @user = User.find(params[:id])
+    each_user
     @user.update(update_params)
     redirect_to :root
   end
@@ -19,6 +19,10 @@ class UsersController < ApplicationController
   def update_params
     params.require(:user).permit(:name,:email,:position,:profile,
       :occupation)
+  end
+
+  def each_user
+    @user = User.find(params[:id])
   end
 
 end
