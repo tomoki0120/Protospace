f97591dc04139d01831706bdb70d71f0ef0ec682
matsuda tomoki <t.m.0011223344@gmail.com>
2016-09-09 16:08:08 +0900

    Edit_update_action_on_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,24 @@
 class UsersController < ApplicationController
 
-def edit
-end
 
-def index
-end
+  def show
+    @user = User.find(params[:id])
+  end
 
-def show
-  @user = User.find(params[:id])
-end
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(update_params)
+    redirect_to :root
+  end
+
+  private
+  def update_params
+    params.require(:user).permit(:name,:email,:position,:profile,
+      :occupation)
+  end
 
 end
