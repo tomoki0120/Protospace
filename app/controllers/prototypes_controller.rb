a06221afb7fbf75a1385c6be02042ac3897c5c4b
matsuda tomoki <t.m.0011223344@gmail.com>
2016-09-13 15:09:42 +0900

    Fix_protoypes_controller

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -4,8 +4,20 @@ class PrototypesController < ApplicationController
   end
 
   def new
+    @prototypes = Prototype.new
+    @prototypes.images.build
+  end
+
+  def create
+    @prototypes = Prototype.create(create_params)
+    redirect_to :root
   end
 
   def show
+  end
+
+  private
+  def create_params
+    params.require(:proto).permit(:title, :catch_copy, :concept, :user_id, images_attributes: [:id, :image_url, :status])
   end
 end
