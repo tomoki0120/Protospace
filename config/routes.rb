94af37e985b99458a73f6ed2aeb8b178abc058d9
matsuda tomoki <t.m.0011223344@gmail.com>
2016-10-18 16:08:03 +0900

    Add_routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
     resources :likes, only: [:create,:destroy]
     resources :comments, only: [:create,:destroy]
   end
+  resources :populars, only: [:index]
+  resources :newests, only: [:index]
   resources :users, only: [:show,:edit,:update]
 end
